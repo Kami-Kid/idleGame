47e48b9f98983cf1d76375a2a3d3493c5b4ab578
Kami-Kid <55991137+Kami-Kid@users.noreply.github.com>
2022-06-29 15:02:17 +0100

    added start of stat changes

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 class Player{
-    constructor(pClass, Cps, Cpc, pAtk, pDef, pSpd, Hp, atkMult, pRegen, gold){
-        //cps = coins/s, cpc = coins/click, P denotes player, Pregen = hp regen
+    constructor(pClass, Cps, Cpc, pAtk, pDef, pSpd, Hp, maxHp, atkMult, gold, regen){
+        //cps = coins/s, cpc = coins/click, P denotes player, 
         this.pClass = pClass;
         this.Cpc = Cpc;
         this.Cps = Cps;
@@ -8,15 +8,54 @@ class Player{
         this.pDef = pDef;
         this.pSpd = pSpd;
         this.Hp = Hp;
+        this.maxHp = maxHp
         this.atkMult = atkMult;
-        this.pRegen = pRegen;
         this.gold = gold;
+        this.regen = regen;
+        this.permRegen  = regen;
     }
     takeDmg(dmgAmt) {
         this.Hp -= dmgAmt*this.pDef;
     }
     dealDmg(Enemy){
         Enemy.takeDmg(this.pAtk * this.dmgMult)
+    }
+    startBattle(){
+        this.regen = 0.5
+    }
+    endBattle(){
+        this.regen = this.permRegen
+    }
+    addRegen(amt,type){
+        if(type = "potion"){
+            this.regen+=amt
+            return
+        }
+        if(type = "armour"){
+            this.permRegen += amt
+        }
+    }
+    addHp(){
+        if(type = "potion"){
+            this.Hp+=amt
+            return
+        }
+        if(type = "armour"){
+            this.maxHp += amt
+        }
+    }
+    addHp(){
+        if(type = "potion"){
+            this.pDef+=amt
+            return
+        }
+        if(type = "armour"){
+            this.pDef += amt
+        }
+    }
+    changeArmour(stats){
+        this.addRegen(stats.regen, "armour")
+        this.def
     }
 }
 class Enemy{
@@ -30,7 +69,8 @@ class Enemy{
     takeDmg(Player){
         this.eHp -= Player.pAtk*Player.atkMult*this.eDef;
         if(this.eHp <= 0){
-            Player
+            Player.getReward(this.eReward)
+            Player.endBattle()
         }
     }
 }
