20d8fa8adb360e7d13d983b7839d6b281b018d5b
Kami-Kid <55991137+Kami-Kid@users.noreply.github.com>
2022-07-08 00:08:51 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const classes = {
-    warrior: { atk: 8, def: 4, spd: 3, hp: 20, mult: 1.25, regen: 3 }
+    warrior: { atk: 8, def: 0.8, spd: 3, hp: 20, mult: 1.25, regen: 3 }
 }
 const enemies = {
-    bat: { hp: 5, atk: 1, def: 0, spd: 2, reward: 10 }
+    bat: { hp: 5, atk: 1, def: 1.5, spd: 2, reward: 10 },
+    ent: { hp: 50, atk: 2, def: 0.9, spd: 5, reward: 100 }
 }
 class Player {
     constructor(pClass, Cps, Cpc, gold) {
@@ -10,22 +11,25 @@ class Player {
         this.pClass = pClass;
         this.Cpc = Cpc;
         this.Cps = Cps;
-        this.Atk = classes.warrior.atk;
-        this.Def = classes.warrior.def;
-        this.Spd = classes.warrior.spd;
-        this.Hp = classes.warrior.hp;
-        this.maxHp = classes.warrior.hp;
-        this.atkMult = classes.warrior.mult;
+        this.Atk = classes[pClass].atk;
+        this.Def = classes[pClass].def;
+        this.Spd = classes[pClass].spd;
+        this.Hp = classes[pClass].hp;
+        this.maxHp = classes[pClass].hp;
+        this.atkMult = classes[pClass].mult;
         this.gold = gold;
-        this.regen = classes.warrior.regen;
+        this.regen = classes[pClass].regen;
         this.inBattle = 0;
         this.atkCharge = 0;
     }
     takeDmg(dmgAmt) {
         this.Hp -= dmgAmt * this.Def;
+        if (this.Hp <= 0) {
+            this.endBattle()
+        }
     }
     dealDmg(Enemy) {
-        Enemy.takeDmg(this.Atk * this.dmgMult)
+        Enemy.takeDmg(this)
     }
     startBattle() {
         this.inBattle = 1
@@ -77,16 +81,16 @@ class Player {
 }
 class Enemy {
     constructor(enemyType) {
-        this.Atk = enemies.enemyType.atk;
-        this.Def = enemies.enemyType.def;
-        this.Hp = enemies.enemyType.hp;
-        this.Reward = enemies.enemyType.reward;
-        this.Spd = enemies.enemyType.spd;
+        this.Atk = enemies[enemyType].atk;
+        this.Def = enemies[enemyType].def;
+        this.Hp = enemies[enemyType].hp;
+        this.Reward = enemies[enemyType].reward;
+        this.Spd = enemies[enemyType].spd;
         this.atkCharge = 0;
     }
     takeDmg(Player) {
         this.Hp -= Player.Atk * Player.atkMult * this.Def;
-        if (this.eHp <= 0) {
+        if (this.Hp <= 0) {
             Player.getReward(this.Reward)
             Player.endBattle()
         }
@@ -94,34 +98,69 @@ class Enemy {
     dealDmg(Player) {
         Player.takeDmg(this.Atk)
     }
+    resetCharge() {
+        this.atkCharge = 0;
+    }
 }
 
 
-const player = new Player(warrior, 1, 5, 0)
+const player = new Player("warrior", 1, 5, 0)
 
 function beginBattle(Player, Enemy) {
     Player.startBattle()
     if (Player.inBattle == 1) {
-        setTimeout(battleTurn, 500, [Player, Enemy])
+        setTimeout(() => battleTurn(Player, Enemy), 500)
     }
 }
+var currEnemy
 
 function battleTurn(Player, Enemy) {
-    Player.atkCharge++
-        Enemy.atkCharge++
-        if (Player.atkCharge <= Player.Spd) {
+    currentStage = 0
+    currEnemy = Enemy
+    console.log(Enemy)
+    console.log(Player)
+    if (Player.inBattle) {
+        Player.atkCharge += 1;
+        if (Player.atkCharge >= Player.Spd) {
+            Player.atkCharge = 0;
             Player.dealDmg(Enemy)
         }
-    if (Enemy.atkCharge <= Enemy.Spd) {
-        Enemy.dealDmg(Player)
+        Enemy.atkCharge += 1;
+        if (Enemy.atkCharge >= Enemy.Spd) {
+            Enemy.resetCharge()
+            Enemy.dealDmg(Player)
+        }
+        return setTimeout(() => battleTurn(Player, Enemy), 500)
     }
 
 }
 
-function updateUI() {
-    document.querySelector(".hpUI").innerHTML = player.Hp + "/" + player.maxHp
-    document.querySelector(".atkUI").innerHTML = player.Atk
-}
-setInterval(updateUI, 1000)
+currentStage = 0
 
-document.querySelector("#battleInit").addEventListener("click", updateUI(player, new Enemy("bat")))
+function updateUI() {
+    document.querySelector("#hpUI").innerHTML = "Hp: " + Math.floor(player.Hp) + "/" + player.maxHp
+    document.querySelector("#atkUI").innerHTML = "Atk: " + player.Atk
+    if (player.inBattle) {
+
+        document.querySelector("#colours").setAttribute("height", 15)
+        document.querySelector("#eHp").innerHTML = Math.floor(currEnemy.Hp)
+        document.querySelector("#colours").setAttribute("width", 500 - currentStage)
+        currentStage += 10
+        console.log(document.querySelector("#colours").getAttribute("width"))
+        return
+    }
+    document.querySelector("#colours").setAttribute("height", 0)
+}
+
+
+
+updateUI()
+setInterval(updateUI, 10)
+
+var test;
+
+function exampleBattle() {
+    test = new Enemy("ent")
+    beginBattle(player, test)
+}
+//document.getElementById("battleInit").addEventListener("click", beginBattle(player, new Enemy("bat")))
